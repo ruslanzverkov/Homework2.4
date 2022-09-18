0909f87a6150cda9f5b81ec878b4738216354ebb
ruslanzverkov <createmed@mail.ru>
2022-09-18 22:50:00 +0300

    3

--- src/main/java/Skypro/student/Main.java
+++ src/main/java/Skypro/student/Main.java
@@ -5,19 +5,39 @@ import Skypro.student.exeption.WrongPasswordException;
 
 public class Main {
     public static void main(String[] args) {
-        String login = "java_skypro.go";
+        String login = "java_skypro.gojava_skypro.gojava_skypro.go";
         String password = "D_1hWiKjjP_9";
-        String confirmPassword = "D_1hWiKjjP_9";
+        String confirmPassword = "D_1hWiKjjP_";
         System.out.println(acceptLoginPassword(login, password, confirmPassword));
     }
 
     public static boolean acceptLoginPassword(String login, String password, String confirmPassword) {
+
+        boolean containsSymbolsLogin = containsSymbols(login);
+        boolean conSymbolsPassword = containsSymbols(password);
+        boolean checkLoginLengthLogin = checkLoginLength(login);
+        boolean checkLoginLengthPassword = checkPasswordLength(password);
+        boolean equalsPasswordConfirmPassword = equalsPasswordConfirmPassword(password, confirmPassword);
+        try {
+
+            containsSymbols(login);
+            containsSymbols(password);
+            checkLoginLength(login);
+            checkPasswordLength(password);
+            equalsPasswordConfirmPassword(password, confirmPassword);
+        } catch (WrongLoginException|WrongPasswordException  e)
+        //Видно что есть 2 ошибки, 2я не выводиться непойму
+        //что надо сделать??
+        {
+            System.out.println(e.getMessage());
+            return false;
+        }
+
         return containsSymbols(login)
                 & containsSymbols(password)
                 & checkLoginLength(login)
                 & checkPasswordLength(password)
                 & equalsPasswordConfirmPassword(password, confirmPassword);
-
     }
 
     public static boolean containsSymbols(String symbols) {
@@ -28,7 +48,7 @@ public class Main {
         return false;
     }
 
-    public static boolean checkLoginLength(String login) {
+    public static boolean checkLoginLength(String login) throws WrongLoginException {
         if (login.length() > 20) {
             throw new WrongLoginException("Логин слишком длинный-%s");
         }
@@ -43,8 +63,9 @@ public class Main {
         return false;
     }
 
-    public static boolean equalsPasswordConfirmPassword(String password, String confirmPassword) {
-        if (password.equals(confirmPassword))return true;
+    public static boolean equalsPasswordConfirmPassword(String password, String confirmPassword)
+            throws WrongPasswordException {
+        if (password.equals(confirmPassword)) return true;
         throw new WrongPasswordException("Пароль не совпадает");
 
     }
